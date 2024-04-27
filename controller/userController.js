23b0be2fa5e65c9f08a50d2ca42bcddbe443308b
Suhail TH <suhailth17756@gmail.com>
2024-04-27 08:46:24 +0530

    api changes added

--- controller/userController.js
+++ controller/userController.js
@@ -3,7 +3,7 @@
 
     const postlogin=(req,res)=>{
         try {
-            console.log(req.body);
+          
                 console.log("hii post login called")
                 res.send({message:"somthing added successfully"})
         } catch (error) {
