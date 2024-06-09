1048350214e0a7912e9bf83dbd05a2f0d67f3b2a
Suhail TH <suhailth17756@gmail.com>
2024-06-09 11:19:30 +0530

    changes made to the route and controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,15 +1,12 @@
+const postlogin = (req, res) => {
+  try {
+    console.log(rew.body);
+    res.send({ message: "somthing added successfully" });
+  } catch (error) {
+    res.status(500).send({ message: "somthing went wrong" });
+  }
+};
 
-
-
-    const postlogin=(req,res)=>{
-        try {
-                res.send({message:"somthing added successfully"})
-        } catch (error) {
-            res.status(500).send({message:"somthing went wrong"})
-        }
-    }
-
-
-    module.exports={
-        postlogin
-    }
+module.exports = {
+  postlogin,
+};
