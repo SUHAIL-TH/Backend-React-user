bfb644a3f9eae22af478e6ecd4cf607d3c464a36
Suhail TH <suhailth17756@gmail.com>
2024-06-14 01:16:42 +0530

    usercontroller

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,24 @@
-const postlogin = (req, res) => {
+const jwt=require("jsonwebtoken")
+const userSchema =require("../model/userSchema")
+const bycrpt=require("bcrypt")
+
+const postlogin = async(req, res) => {
   try {
-    console.log(rew.body);
-    res.send({ message: "somthing added successfully" });
+    
+    console.log(req.body);
+    let user=await userSchema.findOne({email:req.email})
+    if(user){
+
+
+      res.send({ message: "somthing added successfully" });
+    }else{
+      res.status(401).send({message:"User not exsisted",status:false})
+    }
+
     
   } catch (error) {
     console.log(error)
-    res.status(500).send({ message: "somthing went wrong" });
+    res.status(500).send({ message: "somthing went wrong" ,status:true});
   }
 };
 
