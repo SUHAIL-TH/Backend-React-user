cd9390b21846197c2245348b3c18a88512af5048
Suhail TH <suhailth17756@gmail.com>
2024-09-25 12:10:06 +0530

    remove unwanted route

--- routes/user.js
+++ routes/user.js
@@ -9,5 +9,5 @@ userRouter.post("/signup",userController.postsigup)
 userRouter.get("/userdetails",verify,userController.userDetails)
 userRouter.post("/resetemail",userController.resetEmail)
 userRouter.post("/singupcontroller",userController.getUserDetails)
-userRouter.post("/singupcontroller",userController.getUserDetails)
+
 module.exports=userRouter   
